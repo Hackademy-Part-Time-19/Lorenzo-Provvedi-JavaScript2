81da4d90205d1908ee35fd6fde074e5dc27b8090
lory <lory608@gmail.com>
2023-11-18 10:13:00 +0100

    esercitazione 2 javascript

---
+++ javascript2-1.js
@@ -0,0 +1,24 @@
+
+
+
+
+//traccia 1 
+
+let quantità_stock= 30
+
+let ordini= parseInt(prompt("salve,quanti ordini vuole fare?"))
+
+if(ordini>=quantità_stock) {
+    alert("si può fare")
+}
+
+else{
+    alert("unità non disponibili")
+}
+
+
+
+
+
+
+
---
+++ javascript2-2.js
@@ -0,0 +1,169 @@
+
+//traccia 2 
+
+
+
+
+
+
+let v= 0
+
+if(v<18){
+    console.log("insufficiente")
+}
+
+if(v>=18 && v<21){
+    console.log("sufficiente")
+}
+
+if(v>=21 && v<24){
+    console.log("buono")
+}
+
+if(v>=24 && v<27){
+    console.log("distinto")
+}
+
+if(v>=27 && v<=29){
+    console.log("ottimo")
+}
+
+if(v==30){
+    console.log("eccellente")
+}
+
+
+v=20
+
+
+if(v<18){
+    console.log("insufficiente")
+}
+
+if(v>=18 && v<21){
+    console.log("sufficiente")
+}
+
+if(v>=21 && v<24){
+    console.log("buono")
+}
+
+if(v>=24 && v<27){
+    console.log("distinto")
+}
+
+if(v>=27 && v<=29){
+    console.log("ottimo")
+}
+
+if(v==30){
+    console.log("eccellente")
+}
+
+
+v=23
+
+if(v<18){
+    console.log("insufficiente")
+}
+
+if(v>=18 && v<21){
+    console.log("sufficiente")
+}
+
+if(v>=21 && v<24){
+    console.log("buono")
+}
+
+if(v>=24 && v<27){
+    console.log("distinto")
+}
+
+if(v>=27 && v<=29){
+    console.log("ottimo")
+}
+
+if(v==30){
+    console.log("eccellente")
+}
+
+
+v=25
+
+if(v<18){
+    console.log("insufficiente")
+}
+
+if(v>=18 && v<21){
+    console.log("sufficiente")
+}
+
+if(v>=21 && v<24){
+    console.log("buono")
+}
+
+if(v>=24 && v<27){
+    console.log("distinto")
+}
+
+if(v>=27 && v<=29){
+    console.log("ottimo")
+}
+
+if(v==30){
+    console.log("eccellente")
+}
+
+
+v=28
+
+if(v<18){
+    console.log("insufficiente")
+}
+
+if(v>=18 && v<21){
+    console.log("sufficiente")
+}
+
+if(v>=21 && v<24){
+    console.log("buono")
+}
+
+if(v>=24 && v<27){
+    console.log("distinto")
+}
+
+if(v>=27 && v<=29){
+    console.log("ottimo")
+}
+
+if(v==30){
+    console.log("eccellente")
+}
+
+
+v=30
+
+if(v<18){
+    console.log("insufficiente")
+}
+
+if(v>=18 && v<21){
+    console.log("sufficiente")
+}
+
+if(v>=21 && v<24){
+    console.log("buono")
+}
+
+if(v>=24 && v<27){
+    console.log("distinto")
+}
+
+if(v>=27 && v<=29){
+    console.log("ottimo")
+}
+
+if(v==30){
+    console.log("eccellente")
+}
